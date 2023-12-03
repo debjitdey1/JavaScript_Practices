f95b22bf1a37e689d0b7a832af65f9bb197025dc
Debjit Dey <121121842+debjitdey1@users.noreply.github.com>
2023-12-03 04:11:28 +0000

    variable_Declare

---
+++ 01_practices/01_veriable.js
@@ -0,0 +1,20 @@
+const accountId = 123456789;
+let accountEmail = "debjitdey559@gmail.com"; // int, var, const this is for a keyword
+var accountNumber = "197356";
+accountPassword = "4455544";
+let acc;
+
+// accountId = 658458566;
+accountEmail = "debjitdey@outlook.com";
+accountNumber = "155544";
+accountPassword = "565646";
+
+/*
+Prefer not use the var
+because bock scoope is functional scoope
+*/
+
+console.log(accountId);
+console.table([accountEmail, accountId, accountNumber, accountPassword, acc]);
+
+//Do not Use the Var Because Not allowed the javaScript advance level cause the use is SCOOPE and the SCoope mens {} this is symbol
