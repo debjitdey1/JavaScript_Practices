292a9beb57ea60d873e625a4171006d49aaabcf9
debjitdey1 <debjitdey559@gmail.com>
2023-12-20 09:51:44 +0530

    operations

---
+++ 01_practices/02_vriables.js
@@ -0,0 +1,21 @@
+// veriables To be declayer
+
+let name = "Debjit"; // string DataType
+let age = 20; //number DataType
+let isLoggedIn = false; //boolean DataType
+let state;
+
+// PREMETIVE DATATYPE
+
+// number => 2 to power of  53
+// bigint
+//string ==> ""
+// boolean ==> true / false
+// null ==> standalone Value
+// undefined ==>
+// symbol ==> unique
+
+//object
+
+console.log(typeof null); // output - object
+console.log(typeof undefined); // output - undefined
---
+++ 01_practices/03_conversionOperations.js
@@ -0,0 +1,43 @@
+let score = "65"; // this is call string value
+
+console.log(typeof score); // output --> string
+
+let valueInNumber = Number(score); // string value is converted to be Number  is to be call...
+console.log(typeof valueInNumber); // output --> number
+console.log(valueInNumber);
+
+// let score = "65abc"
+
+//console.log(score);           --> output =>  NaN ("Not a Number")
+
+let isBoolean = "hitesh";
+
+let booleanIsLoggedIn = Boolean(isBoolean);
+console.log(booleanIsLoggedIn);
+
+//doumentaion
+
+// 1 ==> true; 0 ==> False
+// "" ==> false;
+// "Debjit" ==> True;
+
+let someNumber = 54; // call Number
+
+let stringNumber = String(someNumber);
+console.log(stringNumber); //You can see the number but that is the "String"
+console.log(typeof stringNumber); // pass for  output in String
+
+/*
+let someNull = 54;          // call Number
+
+let nullNumber = null(someNull); // you can define the null of the charecter / number ....Theare is  output will be ==> "null is not a function".
+console.log(nullNumber); //You can see the number but that is the "String"
+
+*/
+
+//********************************** OPERATIONS **********************************//
+
+let value = 8;
+//then value will be output in "-8"
+let negValue = -value;
+console.log(negValue);
