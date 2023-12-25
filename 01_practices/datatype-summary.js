9b871e66114d4530873165636c0d4b734882dc1e
debjitdey1 <debjitdey559@gmail.com>
2023-12-25 23:03:12 +0530

    Datatype-Summary

---
+++ 01_practices/datatype-summary.js
@@ -0,0 +1,20 @@
+const id = Symbol("123");
+const idNumber = Symbol("123");
+
+console.log(id == idNumber);
+console.log(id);
+console.log(idNumber);
+
+const bigNumber = 5555555669325556325563225566646874561577565656n; // n mens BIgINT
+console.log(bigNumber);
+
+const heroes = ["Spiderman", "Ironman", "Captain America"];
+let myobj = {
+  name: "Debjit",
+  age: 21,
+};
+console.log([heroes, myobj]);
+console.log(typeof idNumber);
+
+let place = null;
+console.log(typeof place); //That is the "Null" Type of Value is call for =="OBJECT"
---
+++ 01_practices/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+const id = Symbol("123");
+const idNumber = Symbol("123");
+
+console.log(id == idNumber);
+console.log(id);
+console.log(idNumber);
