cb01e53f1b17402d8df709b4723f2e319e85f73e
debjitdey1 <debjitdey559@gmail.com>
2023-12-27 21:42:06 +0530

    datatype_summay

--- 01_practices/datatype-summary.js
+++ 01_practices/datatype-summary.js
@@ -1,20 +1,38 @@
-const id = Symbol("123");
-const idNumber = Symbol("123");
+// const id = Symbol("123");
+// const idNumber = Symbol("123");
 
-console.log(id == idNumber);
-console.log(id);
-console.log(idNumber);
+// console.log(id == idNumber);
+// console.log(id);
+// console.log(idNumber);
 
-const bigNumber = 5555555669325556325563225566646874561577565656n; // n mens BIgINT
-console.log(bigNumber);
+// const bigNumber = 5555555669325556325563225566646874561577565656n; // n mens BIgINT
+// console.log(bigNumber);
 
-const heroes = ["Spiderman", "Ironman", "Captain America"];
-let myobj = {
-  name: "Debjit",
-  age: 21,
+// const heroes = ["Spiderman", "Ironman", "Captain America"];
+// let myobj = {
+//   name: "Debjit",
+//   age: 21,
+// };
+// console.log([heroes, myobj]);
+// console.log(typeof idNumber);
+
+// let place = null;
+// console.log(typeof place); //That is the "Null" Type of Value is call for =="OBJECT"
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+let myname = "Debjit Dey";
+
+let changename = myname;
+changename = "Dustu";
+console.log(changename); // output:  Debjit Dey
+console.log(myname);
+
+let userOne = {
+  accno: 2254658426652,
+  ifsc: "contai",
 };
-console.log([heroes, myobj]);
-console.log(typeof idNumber);
-
-let place = null;
-console.log(typeof place); //That is the "Null" Type of Value is call for =="OBJECT"
+let userTwo = userOne;
+userTwo.ifsc = "kolkata";
+console.log(userTwo.ifsc);
+console.log(userOne.ifsc);
