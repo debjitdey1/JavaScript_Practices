f3df282d7420081ec3d2504f3c5f7b74125cd21d
debjitdey1 <debjitdey559@gmail.com>
2024-01-18 15:57:22 +0530

    object

---
+++ 02_Basic/03_object.js
@@ -0,0 +1,32 @@
+//object
+
+//object letarals
+
+const mysym = Symbol("Key1");
+//defined to the symbol and and put the value is object;   Ans:  [Symbol(Key1)]: 'key1';
+const jsUser = {
+  name: "Debjit",
+  [mysym]: "key1", //Symbol
+  ph_no: 5454654,
+  age: 20,
+  email: "debjitdey559@google.com",
+  collage: "Techno India Saltlake",
+}; ///that is call Object AND object are the Keys Value Pair
+
+console.log(jsUser);
+console.log(jsUser.collage);
+console.log(jsUser["email"]); // by  default Email is the String is call of the "";
+console.log(jsUser[mysym]);
+
+jsUser.email = "dejeetdey669@gmail.com";
+Object.freeze(jsUser);
+jsUser.email = "deydebjitmicrsoft.com";
+jsUser.ph_no = 7454565;
+
+console.log(jsUser);
+
+jsUser.greeting = function () {
+  console.log("Hello Js Users");
+};
+
+console.log(jsUser.greeting); //greeting method is call of the output "UNDEFIND";
