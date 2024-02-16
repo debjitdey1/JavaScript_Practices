2d9e9b661da19ff22429fe6c8cbf8b5ffc4c6c48
debjitdey1 <debjitdey559@gmail.com>
2024-02-16 20:34:36 +0530

    If_Statements

---
+++ 04_logicflow/one.js
@@ -0,0 +1,19 @@
+// if
+// "===" that is call striped Equal
+if (2 != 3) {
+  console.log("Exicuted");
+}
+
+// const temparature = 29;
+// if (temparature == 37) {
+//   console.log("Temparature less than 37");
+// } else {
+//   console.log("Temparature Grater than 37");
+// }
+
+const score = 156;
+if (score > 100) {
+  let power = "Fly"; // let besicall use blocked scope elements and VAR beically use the Global Scope Elements
+  console.log(`User power: ${power}`);
+}
+// console.log(`User power: ${power}`);
