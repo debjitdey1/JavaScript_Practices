d9024272dca6e62d1c2a2d227d98565c1e13aa77
debjitdey1 <debjitdey559@gmail.com>
2024-02-17 22:41:15 +0530

    one

--- 04_logicflow/one.js
+++ 04_logicflow/one.js
@@ -16,6 +16,7 @@ if (score > 100) {
   let power = "Fly"; // let besicall use blocked scope elements and VAR beically use the Global Scope Elements
   console.log(`User power: ${power}`);
 }
+
 // console.log(`User power: ${power}`);
 
 const balance = 5000;
@@ -31,4 +32,19 @@ if (balanced < 500) {
   console.log("less than 1500");
 } else {
   console.log("less than 2100");
+}
+
+//Condition check "&&" and "||"
+const userLoggedIn = true;
+const userDebitCard = true;
+const loggedInFromGoogle = true;
+const loggedInFromEmail = true;
+//UserLoggedIn && userDebitcard
+
+if (userLoggedIn && userDebitCard) {
+  console.log("Allow to The Buy The Cource");
+}
+
+if (loggedInFromEmail || loggedInFromGoogle) {
+  console.log("Enter for The Browsers");
 }
