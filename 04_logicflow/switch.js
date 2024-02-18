7b413b8c95780336916b42747938462e00bcd975
debjitdey1 <debjitdey559@gmail.com>
2024-02-18 22:25:12 +0530

    switch

---
+++ 04_logicflow/switch.js
@@ -0,0 +1,16 @@
+const month = 3;
+switch (month) {
+  case 1:
+    console.log("January");
+    break;
+  case 2:
+    console.log("February");
+    break;
+  case 3:
+    console.log("March");
+    break;
+
+  default:
+    console.log("No Months");
+    break;
+}
