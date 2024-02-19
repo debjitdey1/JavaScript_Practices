d4225c848a5349611c1482523f381ed1f36589f6
debjitdey1 <debjitdey559@gmail.com>
2024-02-19 22:54:39 +0530

    truthy

---
+++ 04_logicflow/truthy.js
@@ -0,0 +1,10 @@
+const userEmail = "";
+if (userEmail) {
+  console.log("Got User Email");
+} else {
+  console.log("Don't have user Email");
+}
+
+// Falsy Values
+
+// false, 0, -0, BigInt 0n, "", Null, Undefind, Nan     --> That is the Falsy VAlue
