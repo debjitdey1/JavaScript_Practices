b27ee9b7b7faceef7b7251e08c6d05bec654f440
debjitdey1 <debjitdey559@gmail.com>
2024-02-21 23:07:02 +0530

    For

---
+++ 05_Ittaration/one.js
@@ -0,0 +1,14 @@
+//For
+
+// for (let index = 0; index < 10; index++) {
+//   const element = index;
+//   console.log(element);
+// }
+
+for (let i = 0; i <= 10; i++) {
+  const element = i;
+  if (element == 5) {
+    console.log("5 Is the Biggest Number");
+  }
+  console.log(element);
+}
